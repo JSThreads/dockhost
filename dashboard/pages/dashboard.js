7ea183b6c83d7bc7af055935b15d040347de45d5
Jasiukiewicz Tymon <73474137+jasiukiewicztymon@users.noreply.github.com>
2023-01-26 11:25:04 +0100

    Update dashboard.js

--- dashboard/pages/dashboard.js
+++ dashboard/pages/dashboard.js
@@ -1,10 +1,12 @@
-import Head from 'next/head'
 import styles from '../styles/Dashboard.module.css'
+
+import SectionHeader from '../components/sectionHeader'
 
 export default function Home() {
   return (
     <>
-        HI
+      <SectionHeader></SectionHeader>
+      HIf
     </>
   )
 }
